949797212755787d5677c77ebd9d42507a8b1e6f
Petya <petya200606@gmail.com>
2023-08-02 16:02:21 +0600

    First task

---
+++ hometask2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Add fisrt number");
+ int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Add second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Max = first");
+   Console.WriteLine("Min = second");
+}
+ if (number1 < number2)
+{
+  Console.WriteLine("Max = second");
+  Console.WriteLine("Min = first");
+}
